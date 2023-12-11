b96c831481b268e003f9495ffba142d17b79bc82
BrunaSaNu <bruna.santana.0511@gmail.com>
2023-12-11 02:36:17 -0300

    atividades JAVA05- Laços condicionais

---
+++ src/atividadesPraticasGen/lista1.java
@@ -0,0 +1,132 @@
+package atividadesPraticasGen;
+import java.util.Scanner;
+
+public class lista1 {
+	
+
+	public static void main(String[] args) {
+		//atividade A
+		/*int a = 2;
+		int b = 4;
+		int c = 5;
+		int soma = (a + b);
+		
+		if(soma > c) {
+			System.out.println(a + " + " + b + " = " + soma + " > " + c + "\n" + "A soma de A + B é maior do que C");
+		} else {
+			System.out.println("A soma de A + B é igual a: " + soma + " que não é maior que: " + c);
+		}*/
+		
+		//atividade B
+		
+		/*int a = 2;
+		int b = 2;
+		int c = 5;
+		int soma = (a + b);
+		
+		if(soma > c) {
+			System.out.println(a + " + " + b + " = " + soma + " > " + c + "\n" + "A soma de A + B é maior do que C");
+		} else {
+			System.out.println(a + " + " + b + " = " + soma + " < " + c + "\n" + "A soma de A + B é menor do que C");
+		}*/
+		
+		/*int a = 2;
+		int b = 2;
+		int c = 4;
+		int soma = (a + b);
+		
+		if(soma > c) {
+			System.out.println(a + " + " + b + " = " + soma + " > " + c + "\n" + "A soma de A + B é maior do que C");
+		} else {
+			System.out.println(a + " + " + b + " = " + soma + " = " + c + "\n" + "A soma de A + B é igual a C");
+		}*/
+		
+		
+		//Atividade 2
+		
+		/*Scanner leia = new Scanner(System.in);
+		
+		int numero;
+		
+		System.out.println("Digite um número: ");
+		numero = leia.nextInt();
+		
+		if (numero >= 1 && numero % 2 == 0) {
+            System.out.println("O número " + numero + " é par e positivo!");
+        } else if (numero <= -1 && numero % 2 == 1) {  
+            System.out.println("O número " + numero + " é ímpar e negativo!");  
+        } else if (numero <= -1 && numero % 2 == 0) {  
+            System.out.println("O número " + numero + " é par e negativo");  
+        } else {
+            System.out.println("O número " + numero + " é ímpar e positivo");
+            */
+		
+		//Atividade 3
+		
+		/*Scanner leia = new Scanner(System.in);
+		
+		String nome;
+		int idade;
+		boolean primeira;
+		
+		System.out.println("Digite seu nome: ");
+		nome = leia.nextLine();
+		System.out.println("Digite sua idade: ");
+		idade = leia.nextInt();
+		System.out.println("Já doou sangue? True or false? ");
+		primeira = leia.nextBoolean();
+		
+		if(idade >= 18 && idade <= 59) {
+			System.out.println(nome + " está apto para doar sangue!");
+		} else if (idade >= 60 && idade <= 69 && primeira) {
+			System.out.println(nome + " está apto para doar sangue!");
+		} else {
+			System.out.println(nome + " não está apto para doar sangue!");
+		}
+		*/
+		
+		
+		//Atividade 4
+		
+		/*Scanner leia = new Scanner(System.in);
+		
+        System.out.println("Digite a primeira característica (vertebrado/invertebrado): ");
+        String caracteristica1 = leia.nextLine();
+
+        System.out.println("Digite a segunda característica (mamífero/inseto): ");
+        String caracteristica2 = leia.nextLine();
+
+        System.out.println("Digite a terceira característica (onívoro/herbívoro): ");
+        String caracteristica3 = leia.nextLine();
+
+        if (caracteristica1.equals("vertebrado")) {
+            if (caracteristica2.equals("mamífero")) {
+                if (caracteristica3.equals("onívoro")) {
+                    System.out.println("O animal é um Homem.");
+                } else {
+                    System.out.println("Características não correspondem a um animal conhecido.");
+                }
+            } else {
+                System.out.println("Características não correspondem a um animal conhecido.");
+            }
+        } else if (caracteristica1.equals("invertebrado")) {
+            if (caracteristica2.equals("inseto")) {
+                if (caracteristica3.equals("herbívoro")) {
+                    System.out.println("O animal é uma Lagarta.");
+                } else {
+                    System.out.println("Características não correspondem a um animal conhecido.");
+                }
+            } else {
+                System.out.println("Características não correspondem a um animal conhecido.");
+            }
+        } else {
+            System.out.println("Características não correspondem a um animal conhecido.");
+        }
+
+        leia.close();
+		*/
+		
+		
+        }
+    }
+
